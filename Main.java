e005bb486cfeae65c9bd3c673213c6275a7ab456
Cz9911 <>
2021-04-20 14:35:16 +0000

    I added the scanner named scanner.

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter Your password below.");
+    Scanner scanner = new Scanner (System.in);
+    
   }
 }
