9a28f40a2b6694cdcffb0228655b8c5a96eeca06
Cz9911 <>
2021-04-26 22:42:32 +0000

    I added proper syntax/fixed password verification.

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    String password = "@enter.com";
+    Scanner scanner = new Scanner (System.in);
     System.out.println("Enter Your password below.");
+    String passInput = scanner.next();
+    if(passInput.equals(password)){
+      System.out.println("Welcome");
+    }
+    else{
+      System.out.println("Get atta here!");
+    }
+
+    
+    scanner.close(); 
   }
 }
